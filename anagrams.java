e8ac26f026c1ea89f779dc0bca9197a409eb5430
anindo69 <53431453+anindo69@users.noreply.github.com>
2021-03-06 19:12:51 +0600

    Add files via upload

---
+++ anagrams.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class anagrams {
+
+    static boolean isAnagram(String a, String b) {
+        //remove space and turning to lowercase
+        a = a.replaceAll("\\s","").toLowerCase();
+        b = b.replaceAll("\\s","").toLowerCase();
+        
+        //cheacking length
+        if(a.length()!=b.length()){
+            return false;
+        }else{
+            for (int i=0;i<a.length();i++){
+                for(int j=0;j<b.length();j++){
+                    if(a.charAt(i)==b.charAt(j)){
+                        b = b.substring(0,j)+b.substring(j+1);
+                        break;
+                    }
+                }
+            }
+            if(b.length()==0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
